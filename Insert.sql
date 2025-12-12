92dc925cb53dd77eeb405e7adc610287e41cede5
Huỳnh Tấn Phát <phat76997@gmail.com>
2025-12-13 00:13:05 +0700

    insert

---
+++ Insert.sql
@@ -0,0 +1,27 @@
+create database INSERTT;
+
+create table insertt;
+use insertt;
+
+insert into customers (id,name , age,address,salary)
+value (1 , 'Ha Anh' ,32,'Da Nang',20000.000);
+
+insert into customers (id,name , age,address,salary)
+value (2 , 'Van Ha' ,25,'Ha Noi',15000.000);
+
+insert into customers (id,name , age,address,salary)
+value (3 , 'Vu Bang' ,23,'Vinh',2000.000);
+
+insert into customers (id,name , age,address,salary)
+value (4 , 'Thu Minh' ,25,'Ha Noi',65000.000);
+
+insert into customers (id,name , age,address,salary)
+value (5 , 'Hai An' ,27,'Ha Noi',8500.000);
+
+insert into customers (id,name , age,address,salary)
+value (6 , 'Hoang' ,22,'Ha Noi',4500.000)
+;
+insert into customers (id,name , age,address,salary)
+value (7 , 'Binh' ,24,'Ha Noi',10000.000);
+
+insert into customers value(7,'Phat',22,'HCM',1000.000);
